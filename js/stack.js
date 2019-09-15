f4bc1afea20ed56b081e977686199bd15b16ce4e
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-09-14 17:17:53 -0700

    practice of data structures: stack

---
+++ js/stack.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  insert(data) {
+    this.items.push(data);
+  }
+
+  remove() {
+    this.items.pop();
+  }
+
+  peek(data) {
+    this.items.includes(data);
+  }
+}
+
+const isValid = text => {};
