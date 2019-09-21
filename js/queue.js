fdc18f307f0ea05a9a2273bb43bc353cf1809b79
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-09-21 07:29:55 -0700

    stone_jewels implementation in js hackerank

--- js/queue.js
+++ js/queue.js
@@ -17,23 +17,15 @@ class CircularQueue {
   }
 
   enqueue(item) {
-    // moves  write pointer
+    // moves write pointer
     // check if queue is full - the write pointer has reached the end of the array
     // check if q is empty or less than end of array
 
     if (this.write === this.queueLength()) {
       if (this.read) {
         this.write = 0;
-
-        if (this.write < this.read + 2 /* this.write + 1 === this.read */) {
-          items[this.write] = item;
-          this.write += 1;
-          this.filled += 1;
-        } /* this.write + 1 === this.read || this.write === this.read - 1 */ else {
-          throw new Error("Queue is full");
-        }
       } else {
-        throw new Error("Queue is full");
+        throw new Error("Queue is full"); // [W(),_,R(d),e,f,g]
       }
     } else {
       items[this.write] = item;
@@ -42,5 +34,17 @@ class CircularQueue {
     }
   }
 
-  dequeue() {}
+  dequeue() {
+    // moves read pointer
+    // check if q is empty
+    // check if queue is full - the write pointer has reached the end of the array
+
+    if (this.write === this.read) {
+      throw new Error("Queue is empty"); // [W(),_,R(d),e,f,g]
+    } else {
+      items[this.read] = null;
+      this.read -= 1;
+      this.filled -= 1;
+    }
+  }
 }
