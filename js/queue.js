36a66c51740d13ec9d46d21a9d6f3bb89ede5e23
Julie Hutchinson <julie.gomez001@mymdc.net>
2019-09-22 10:03:19 -0700

    circular buffer with an array implementation

--- js/queue.js
+++ js/queue.js
@@ -16,21 +16,47 @@ class CircularQueue {
     return this.items.length - 1;
   }
 
+  isAtTheEnd(pointer) {
+    return pointer === this.queueLength();
+  }
+
+  isEmpty() {
+    return this.read === this.write;
+  }
+
+  isPointerWrapping(point, limiter = this.read) {
+    return this.isAtTheEnd(point) && !limiter;
+  }
+
+  isFull(pter, limit = this.read) {
+    return pter < limit - 1;
+  }
+
   enqueue(item) {
     // moves write pointer
-    // check if queue is full - the write pointer has reached the end of the array
+    // check if queue is full - the write pointer is behind the read pointer
     // check if q is empty or less than end of array
+    // [A, B(W), C, D(R)]
+    // [A, [](W), C(R), D]
 
-    if (this.write === this.queueLength()) {
-      if (this.read) {
-        this.write = 0;
-      } else {
-        throw new Error("Queue is full"); // [W(),_,R(d),e,f,g]
-      }
+    if (!this.isPointerWrapping(this.write) || this.isFull(this.write)) {
+      throw new Error("Queue is full");
     } else {
-      items[this.write] = item;
+      this.items[this.write] = item;
+      this.write = this.isPointerWrapping(this.write) ? 0 : (this.write += 1);
+      this.filled += 1;
+    }
+
+    if (!this.isPointerWrapping(this.write) || this.isEmpty()) {
+      this.items[this.write] = item;
       this.write += 1;
       this.filled += 1;
+    } else if (this.isFull()) {
+      this.items[this.write] = item;
+      this.filled += 1;
+      this.write = 0;
+    } else {
+      throw new Error("Queue is full");
     }
   }
 
@@ -38,12 +64,17 @@ class CircularQueue {
     // moves read pointer
     // check if q is empty
     // check if queue is full - the write pointer has reached the end of the array
+    // e/c what happens with read is at the end
 
-    if (this.write === this.read) {
-      throw new Error("Queue is empty"); // [W(),_,R(d),e,f,g]
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty"); // [a, (W), , , ,g(R)]
     } else {
-      items[this.read] = null;
-      this.read -= 1;
+      this.read = this.isAtTheEnd(this.read) ? 0 : (this.read += 1);
+      const indexToDequeue = !this.read
+        ? this.queueLength() - 1
+        : this.read - 1;
+
+      this.items[indexToDequeue] = null;
       this.filled -= 1;
     }
   }
