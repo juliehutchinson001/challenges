c4e41ea9163481cc0bb6f40c8e519c6030ba1573
Julie Hutchinson <julie.gomez001@mymdc.net>
2020-01-12 13:11:43 -0800

    Fix bugs and make sorting more performant

--- js/merge_sort.js
+++ js/merge_sort.js
@@ -1,30 +1,48 @@
+/**
+ * `mergeFunction` takes care of sorting the shorter vertions of the original arr
+ * @param {Int[]} `leftArr` smaller partition of the left side of an array
+ * @param {Int[]} `rightArr` smaller partition of the right side of an array
+ */
 const mergeFunction = (leftArr, rightArr) => {
+  // [2,8], [5,3]
   const tempArr = [];
-  const lastPosOfArr = !tempArr.length ? 0 : tempArr.length;
+  let lIdx = 0;
+  let rIdx = 0;
 
-  const areArraysNotEmpty = leftArr.length && rightArr.length;
-
-  while (areArraysNotEmpty) {
-    if (leftArr[0] > rightArr[0]) {
-      tempArr[lastPosOfArr] = rightArr[0];
-      rightArr.shift();
+  while (lIdx < leftArr.length && rIdx < rightArr.length) {
+    // 2, 2
+    if (leftArr[lIdx] > rightArr[rIdx]) {
+      // 2 > 5 === false
+      tempArr.push(rightArr[rIdx]);
+      rIdx += 1;
+      console.log(`right array has changed: ${rightArr}`);
     } else {
-      tempArr[lastPosOfArr] = leftArr[0];
-      leftArr.shift();
+      tempArr.push(left.Arr[0]);
+      lIdx += 1;
+      console.log(`left array has changed: ${leftArr}`);
     }
   }
 
-  while (leftArr.length) {
-    tempArr[lastPosOfArr] = leftArr[0];
-    leftArr.shift();
+  while (lIdx < leftArr.length) {
+    tempArr.push(left.Arr[0]);
+    lIdx += 1;
+    console.log(`left array has changed: ${leftArr}\n`);
   }
 
-  while (rightArr.length) {
-    tempArr[lastPosOfArr] = rightArr[0];
-    rightArr.shift();
+  while (rIdx < rightArr.length) {
+    tempArr.push(right.Arr[0]);
+    rIdx += 1;
+    console.log(`right array has changed: ${rightArr}\n`);
   }
+
+  console.log(`\n\ntempArr after sorted: ${tempArr}\n\n`);
+  return tempArr;
 };
 
+/**
+ * `mergeSort` calls itself recursively to partition an arr in smaller parts
+ * @param {Int[]int} `arr` The original array
+ */
 const mergeSort = arr => {
   const lengthOfArr = arr.length;
 
@@ -32,13 +50,14 @@ const mergeSort = arr => {
   console.log(`Original array: ${arr}\n\n`);
 
   const leftArray = mergeSort(arr.slice(0, Math.round(lengthOfArr / 2)));
-  console.log(`left partitioned arr: ${leftArray}\n`);
-  const rightArray = mergeSort(arr.slice(Math.round(lengthOfArr / 2) + 1));
+  console.log(`left partitioned arr: ${leftArray} \n`);
+  const rightArray = mergeSort(arr.slice(Math.round(lengthOfArr / 2)));
   console.log(`right partitioned arr: ${rightArray}\n\n`);
 
-  return mergeFunction(leftArray, rightArray);
+  return mergeFunction(leftArray, rightArray); //[2, 8], [5, 3]
 };
 
 (() => {
-  console.log(mergeSort([2, 8, 5, 3, 9, 4, 1, 7]));
+  console.log(mergeSort([2, 8, 5, 3]));
+  // console.log(mergeSort([2, 8, 5, 3, 9, 4, 1, 7]));
 })();
