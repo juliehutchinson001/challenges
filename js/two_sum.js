13bf7cdf856308391aef97c41feaf2108982e777
Julie Hutchinson <julie.gomez001@mymdc.net>
2020-02-17 07:52:30 -0800

    implementation of two sum with a sorted array

--- js/two_sum.js
+++ js/two_sum.js
@@ -13,42 +13,85 @@
 */
 
 /**
- * Helper function that retrieves the key of a passed value in an object
- * @param {Obj{}} obj the cache object
- * @param {Int} val the value in the object
- */
-const getKeyByValue = (obj, val) =>
-  Object.keys(obj).find(key => obj[key] === val);
-
-/**
  * @description finds two numbers in an unsorted array that add up to the target | Time: O(n); Space: O(n).
  * @param {Array[number]} arr | The unsorted array.
- * @param {*} target | the total of two added numbers from the array.
+ * @param {Number} target | the total of two added numbers from the array.
  */
 const twoSumWithCaching = (arr, target) => {
   const cache = {};
   const result = [];
 
   for (let idx = 0; idx < arr.length; idx++) {
-    const NOT_FOUND_INDEX = -1;
     const compliment = target - arr[idx];
-    const keyOfCompliment = parseInt(getKeyByValue(cache, compliment), 10);
 
-    if (keyOfCompliment > NOT_FOUND_INDEX) {
-      result.push([arr[keyOfCompliment], arr[idx]]);
+    // if the key is already in the cache object
+    if (cache[compliment] !== undefined) {
+      result.push([compliment, arr[idx]]);
     } else {
-      cache[idx] = arr[idx];
+      cache[arr[idx]] = idx;
     }
   }
-
   return result;
 };
 
-// console.log(twoSumWithUnsortedArray([-1, 0, 4, 1, 2, -1, -4], 0));
+const unsortedArray = [-1, 0, 4, 1, 2, -1, -4];
+
+console.log(twoSumWithCaching(unsortedArray, 0));
+// console.log(twoSumWithCaching([100, 200, 300, 400, 800, 0], 600));
+
+/**
+ * Helper function that retrieves a sorted array | insertion sort: O(n^2)operations - O(f+n+t)space
+ * @param {Array[Number]} theArr | an unsorted array
+ * @returns {Array[Number]} theArr | same array but sorted.
+ */
+const insertionSort = arr => {
+  let firstPt = 0;
+  let nextPt = 1;
+  let temp;
+
+  while (nextPt < arr.length) {
+    if (arr[firstPt] <= arr[nextPt]) {
+      firstPt += 1;
+      nextPt += 1;
+    } else {
+      temp = arr[nextPt];
+      for (let ctr = firstPt; arr[ctr] > temp; ctr--) {
+        arr[ctr + 1] = arr[ctr];
+        arr[ctr] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+// console.log(insertionSort(unsortedArray));
 
 /**
  * @description finds two numbers in a sorted array that add up to the target | Time: O(2n); Space: O(1).
  * @param {Array[number]} arr | The sorted array.
- * @param {*} target | the total of two added numbers from the array.
+ * @param {Number} target | the total adition of two numbers from the array.
  */
-const twoSumWithSortingArray = (arr, target) => {};
+const twoSumWithSortingArray = (arr, target) => {
+  //sort the array with sorting algorithm (expensive performance)
+  const sortedArray = insertionSort(arr);
+  const resultArray = [];
+  let firstPtr = 0;
+  let laterPtr = sortedArray.length - 1;
+  //loop: add two pointers and compare with the target;
+  while (firstPtr < laterPtr) {
+    const totalSum = sortedArray[firstPtr] + sortedArray[laterPtr];
+
+    if (totalSum === target) {
+      resultArray.push([sortedArray[firstPtr], sortedArray[laterPtr]]);
+      laterPtr -= 1;
+      firstPtr += 1;
+    } else if (totalSum > target) {
+      laterPtr -= 1;
+    } else {
+      firstPtr += 1;
+    }
+  }
+  return resultArray;
+};
+
+console.log(twoSumWithSortingArray(unsortedArray, 0));
